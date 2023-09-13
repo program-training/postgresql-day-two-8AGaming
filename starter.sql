4c8a591f6720f6861f03177632d77c6fa68e059f
Asaf Avraham Itshack Perets <ap770ai@gmail.com>
2023-09-13 11:44:47 +0300

    Targil A

---
+++ starter.sql
@@ -0,0 +1,52 @@
+-- WITH GOD'S HELP
+-- ASAF AI PERETS
+
+-- CREATE SCHEMA UNIVERSITY
+
+-------------1--------------
+-- CREATE TABLE university.students (
+-- student_id SERIAL PRIMARY KEY,
+-- 	first_name TEXT NOT NULL,
+-- 	last_name TEXT NOT NULL,
+-- 	date_of_birth DATE,
+-- 	email TEXT UNIQUE
+-- )
+-- INSERT INTO university.students (first_name,last_name,date_of_birth,email)
+--  VALUES ('John','Doe','1990-01-15','john.doe@example.com'),
+--  ('Jane','Smith','1992-05-20','jane.smith@example.com
+-- '),
+--  ('Michael','Johnson','1991-09-10','michael.johnson@example.com
+-- '),
+--  ('Emily','Davis','1993-03-25','emily.davis@example.com
+-- ')
+
+-------------2--------------
+-- CREATE TABLE university.teachers (
+-- teacher_id SERIAL PRIMARY KEY,
+-- 	first_name TEXT NOT NULL,
+-- 	last_name TEXT NOT NULL,
+-- 	date_of_birth DATE,
+-- 	email TEXT UNIQUE,
+-- department TEXT,
+-- hire_date DATE
+-- )
+-- INSERT INTO university.teachers
+-- (first_name,last_name,date_of_birth,email,department,hire_date)
+-- VALUES 
+-- ('Professor','Anderson','1975-04-08','prof.anderson@example.com','Computer Science','2010-08-15'),
+-- ('Dr. Sarah','Wilson','1980-12-20','sarah.wilson@example.com','Mathematics','2015-05-02'),
+-- ('Mr. James','Brown','1978-06-14','james.brown@example.com','History','2009-11-11')
+
+-------------3--------------
+-- CREATE TABLE university.courses (
+-- 	course_id SERIAL PRIMARY KEY,
+-- 	course_name TEXT NOT NULL,
+-- 	department TEXT,
+-- 	credits NUMERIC
+-- )
+
+-- INSERT INTO university.courses (course_name,department,credits)
+-- VALUES 
+-- ('Introduction to Programming','Computer Science',3),
+-- ('Calculus I','Mathematics',4),
+-- ('World History','History',3)
